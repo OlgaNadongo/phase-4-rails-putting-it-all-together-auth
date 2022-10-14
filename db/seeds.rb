6d2265d55692127d4d3dcbb01735d4f487df6a2c
Olga Nadongo <winnienadongo@gmail.com>
2022-10-14 21:27:08 +0300

    complete lab

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username:"Ariana", password:"12345",image_url:"https://i5.walmartimages.com/asr/7be94a8e-9a5d-4f87-842f-5fe4084138ba.c95d36e140f5e0d492ca632b42e4543c.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF", bio: "The red pepper is a variety of Capsicum annuum.")
